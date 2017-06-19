a4d3f01028c3389c562326aceaea78a6c4413841
tulasiram <tulasiram.gunipati@gmail.com>
2017-06-19 20:42:27 +0530

    Added RMSE,Jacobian Matrix functions, added Predict, update and updateEkf function. In fusionEKF added functions for Q and F matrices. Initializing the kalman filter is pending.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -91,6 +91,27 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+   int noise_ax = 9;
+   int noise_ay = 9;
+
+   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+   previous_timestamp_ = measurement_pack.timestamp_;
+
+   float dt_2 = dt * dt;
+   float dt_3 = dt_2 * dt;
+   float dt_4 = dt_3 * dt;
+
+	//Modify the F matrix so that the time is integrated
+   kf_.F_(0, 2) = dt;
+   kf_.F_(1, 3) = dt;
+
+	//set the process covariance matrix Q
+	kf_.Q_ = MatrixXd(4, 4);
+	kf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+			   0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+			   dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+			   0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +126,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.UpdateEKF()
   } else {
     // Laser updates
+    ekf_.Update();
   }
 
   // print the output
